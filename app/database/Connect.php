7de89b743d6f20a4687a979c2cee591798064782
Lucas Lacerda <111766399+Lacerda018@users.noreply.github.com>
2024-04-23 17:02:35 -0300

    Add files via upload

--- app/database/Connect.php
+++ app/database/Connect.php
@@ -2,17 +2,34 @@
 
 namespace app\database;
 
+use PDOException;
 use PDO;
+use phpseclib3\Common\Functions\Strings;
 
 class Connect
 {
     public static function connect()
-
     {
-        return new PDO('mysql:host=localhost;dbname=google_login', 'root', '', [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        ]);
+        try {
+            $pdo = new PDO('sqlite:google_login.db');
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            return $pdo;
+        } catch (PDOException $e) { 
+            echo "Erro de conexão: " . $e->getMessage();
+            exit();
+        }
     }
 }
 
-//root@127.0.0.1:3306
+        // try {    
+        //     $pdo = new PDO('sqlite:google_login.db');
+        //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     $query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, email TEXT NOT NULL UNIQUE, senha TEXT NOT NULL, avatar TEXT)";
+        //     $pdo->exec($query);
+        //     echo "Connected successfully";
+        // } catch (PDOException $e) {
+        //     echo "Connection failed:" . $e->getMessage();
+        //     exit;
+        // }
+
+        // return new PDO('sqlite:Users\User\Desktop\Dev\testeCrudArquivo\app\database', [PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_OBJ]);
